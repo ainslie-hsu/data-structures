d1d42227f44d6e0e2b858eabace0882ca08ee504
Ainslie Hsu <ainslie.hsu@gmail.com>
2018-08-26 18:29:06 -0700

    Implement unshift method

--- singly-linked-list/SinglyLinkedList.js
+++ singly-linked-list/SinglyLinkedList.js
@@ -59,6 +59,15 @@ class SinglyLinkedList {
   }
 
   unshift(val) {
-
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
   }
 }
