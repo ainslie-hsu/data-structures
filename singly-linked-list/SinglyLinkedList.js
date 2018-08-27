8f726181f171370a04eef3a4e666a9cb625df36f
Ainslie Hsu <ainslie.hsu@gmail.com>
2018-08-26 18:10:33 -0700

    Implement pop method

--- singly-linked-list/SinglyLinkedList.js
+++ singly-linked-list/SinglyLinkedList.js
@@ -14,7 +14,6 @@ class SinglyLinkedList {
 
   push(val) {
     let newNode = new Node(val);
-
     if (!this.head) {
       this.head = newNode;
       this.tail = this.head;
@@ -22,9 +21,28 @@ class SinglyLinkedList {
       this.tail.next = newNode;
       this.tail = newNode;
     }
-
     this.length++;
-
     return this;
+  }
+
+  pop() {
+    if (!this.head) {
+      return undefined;
+    } else {
+      let current = this.head;
+      let newTail = current;
+      while (current.next) {
+        newTail = current;
+        current = current.next;
+      }
+      this.tail = newTail;
+      this.tail.next = null;
+    }
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
   }
 }
