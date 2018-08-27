0fc5c0bb0cef4623952c0aeff9c6944a1d27a112
Ainslie Hsu <ainslie.hsu@gmail.com>
2018-08-26 17:42:59 -0700

    Implement push method

--- singly-linked-list/SinglyLinkedList.js
+++ singly-linked-list/SinglyLinkedList.js
@@ -13,6 +13,18 @@ class SinglyLinkedList {
   }
 
   push(val) {
+    let newNode = new Node(val);
 
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
+    this.length++;
+
+    return this;
   }
 }
